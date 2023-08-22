b1780a9dced68433fd26f3c8d527eb4d19aeaffa
quabey <50772988+quabey@users.noreply.github.com>
2023-08-23 00:10:54 +0200

    make grid

---
+++ convineplace-frontend/src/lib/pixel.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let color = "green";
+</script>
+
+<div class="pixel" style="background-color: {color};" />
+
+<style>
+  .pixel {
+    width: 10px;
+    height: 10px;
+  }
+</style>
--- convineplace-frontend/src/routes/+page.svelte
+++ convineplace-frontend/src/routes/+page.svelte
@@ -1,5 +1,31 @@
 <script>
-  import { goto } from "@app/navigation";
+  import Pixel from "$lib/pixel.svelte";
+  let canvas = [];
+
+  function populateCanvas() {
+    for (let i = 0; i < 100; i++) {
+      canvas.push("green");
+    }
+    console.log(canvas);
+  }
+  populateCanvas();
 </script>
 
-<button on:click={goto("/game")} />
+Place
+{#each canvas as pixel}
+  <div class="grid">
+    <Pixel color={pixel} />
+  </div>
+{/each}
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+    gap: 0p;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+</style>
