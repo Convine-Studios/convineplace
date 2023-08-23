b32b288481a872e18366cba28656c4e4ffe8ccdb
PaulBeetLover <paul.fragezeichen@gmail.com>
2023-08-23 02:51:29 +0200

    colors

--- convineplace-frontend/src/lib/selector.svelte
+++ convineplace-frontend/src/lib/selector.svelte
@@ -43,13 +43,14 @@
     height: 40px;
     padding: 0rem;
     margin: 0.34rem;
+    box-shadow:  2px -2px 2px lightgrey;
+
   }
   .grid {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(8, 1fr);
     width: fit-content;
-    border: solid 3px black;
     padding: 1px;
     margin: 1rem;
   }
