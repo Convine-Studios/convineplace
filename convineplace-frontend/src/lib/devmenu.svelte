3230dd90ff4ba5d84238e51d43a27054ad287805
quabey <50772988+quabey@users.noreply.github.com>
2023-08-23 23:20:42 +0200

    add stuff, do stuff, change stuff

--- convineplace-frontend/src/lib/devmenu.svelte
+++ convineplace-frontend/src/lib/devmenu.svelte
@@ -2,12 +2,12 @@
 	import { Button, Modal, Tabs, TabItem } from 'flowbite-svelte';
 	import { Icon } from 'flowbite-svelte-icons';
 
-	let devModal = false;
+	let devModal = true;
 </script>
 
 <Button on:click={() => (devModal = true)}>Admin Menu</Button>
 
-<Modal bind:open={devModal} outsideclose>
+<Modal bind:open={devModal} size="lg" outsideclose>
 	<script>
 		import { Tabs, TabItem } from 'flowbite-svelte';
 		import { Icon } from 'flowbite-svelte-icons';
@@ -16,22 +16,22 @@
 	<Tabs style="underline">
 		<TabItem open>
 			<div slot="title" class="flex items-center gap-2">
-				<Icon name="user-circle-solid" size="sm" />
-				Profile
+				<Icon name="grid-solid" size="sm" />
+				Dashboard
 			</div>
 			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>Profile:</b>
+				<b>Dashboard:</b>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua.
 			</p>
 		</TabItem>
 		<TabItem>
 			<div slot="title" class="flex items-center gap-2">
-				<Icon name="grid-solid" size="sm" />
-				Dashboard
+				<Icon name="user-circle-solid" size="sm" />
+				Users
 			</div>
 			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>Dashboard:</b>
+				<b>Profile:</b>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua.
 			</p>
@@ -50,12 +50,10 @@
 		<TabItem>
 			<div slot="title" class="flex items-center gap-2">
 				<Icon name="clipboard-solid" size="sm" />
-				Contacts
+				Stats
 			</div>
 			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>Contacts:</b>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+				<b>Pixels placed: </b>
 			</p>
 		</TabItem>
 	</Tabs>
---
+++ convineplace-frontend/src/lib/stats.js
@@ -0,0 +1,2 @@
+import { supabase } from '$lib/supabase';
+
